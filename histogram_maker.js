cf0120c909ab4ee986f6cc8fe8c4466fb57b1cbc
theofficialklpiano <kenneth.h.liu@gmail.com>
2026-06-05 12:39:32 -0400

    Update histogram_maker.js

--- histogram_maker.js
+++ histogram_maker.js
@@ -16,12 +16,29 @@ function makePlot() {
       return; 
     }
 
+    let binWidth = document.getElementById("bin-width").value.trim();
+    if (!binWidth) { 
+      alert("Please enter a value for Bin Width");
+      return;
+    }
+
+    if (binWidth.isNaN()) {
+      alert("Please enter only numbers.");
+      return;
+    }
+
     let trace = {
       x: xValues,
       type: 'histogram',
       marker: {
         color: '#1f77b4',
         line: { color: 'white', width: 1 }
+      },
+      autobinx: false,
+      xbins: {
+        start: Math.min(...xValues), // set minimum bin edge
+        end: Math.max(...xValues),   // set maximum bin edge
+        size: binWidth
       }
     };
 
