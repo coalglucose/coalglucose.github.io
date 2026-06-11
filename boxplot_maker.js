7c61711a5bd4ac8a708f5600d988ab32da065726
theofficialklpiano <kenneth.h.liu@gmail.com>
2026-06-11 12:30:01 -0400

    Create boxplot_maker.js

---
+++ boxplot_maker.js
@@ -0,0 +1,43 @@
+function makePlot() {
+  try {
+    // get input 
+    let yText = document.getElementById("y-values").value.trim(); 
+    if (!yText) { 
+      alert("Please enter Y values."); 
+      return; 
+    } 
+
+    // string -> array 
+    let xValues = xText.split(',').map(val => Number(val.trim())); 
+
+    // check if number 
+    if (xValues.some(isNaN)) { 
+      alert("Please enter only numbers."); 
+      return; 
+    }
+
+    let trace = {
+      y: yValues,
+      type: 'box',
+      marker: {
+        color: '#1f77b4',
+        line: { color: 'white', width: 1 }
+      },
+      boxpoints: 'suspectedoutliers', // how points are shown
+      jitter: 0.3,                    // sets the horizontal spread of the points
+      pointpos: 0
+    }
+
+    let layout = {
+      title: 'Box Plot of Y Values',
+      xaxis: { title: 'Plot' },
+      yaxis: { title: 'Value' }
+    };
+
+    Plotly.newPlot('graph', [trace], layout);
+    
+  } catch(error) { 
+    alert("Invalid input."); 
+    console.log(error); 
+  } 
+}
